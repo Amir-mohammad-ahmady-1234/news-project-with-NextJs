dff7bd88d69baff3d782158729f73456352cd228
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-08-02 14:59:38 -0700

    added backend and fetch news from sqlite localdata base.

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,11 +1,45 @@
-import { DUMMY_NEWS } from "@/dummy-news";
+"use client";
+
 import NewsList from "@/components/news-list/news-list";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [data, setData] = useState([]);
+  const [isloading, setIsloading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function getNews() {
+      setIsloading(true);
+      const response = await fetch("http://localhost:8080/news");
+
+      if (!response.ok) {
+        setIsloading(false);
+        setError("Faild to fetch news");
+      }
+
+      const news = await response.json();
+      setIsloading(false);
+      setData(news);
+    }
+
+    getNews();
+  }, []);
+
+  if (isloading) return <p>loading ...</p>;
+
+  if (error) return <p>{error}</p>;
+
+  let newsContent;
+
+  if (data) {
+    newsContent = <NewsList news={data} />;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
