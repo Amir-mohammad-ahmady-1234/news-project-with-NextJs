0876c79fdaad99a984f19aec2457c949368f3135
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-08-02 15:17:09 -0700

    fetch news convert from client to server side.

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,45 +1,18 @@
-"use client";
-
 import NewsList from "@/components/news-list/news-list";
-import { useEffect, useState } from "react";
 
-export default function NewsPage() {
-  const [data, setData] = useState([]);
-  const [isloading, setIsloading] = useState(false);
-  const [error, setError] = useState("");
+export default async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
 
-  useEffect(() => {
-    async function getNews() {
-      setIsloading(true);
-      const response = await fetch("http://localhost:8080/news");
-
-      if (!response.ok) {
-        setIsloading(false);
-        setError("Faild to fetch news");
-      }
-
-      const news = await response.json();
-      setIsloading(false);
-      setData(news);
-    }
-
-    getNews();
-  }, []);
-
-  if (isloading) return <p>loading ...</p>;
-
-  if (error) return <p>{error}</p>;
-
-  let newsContent;
-
-  if (data) {
-    newsContent = <NewsList news={data} />;
+  if (!response.ok) {
+    throw new Error("Faild to fetch news.");
   }
+
+  const news = await response.json();
 
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 }
